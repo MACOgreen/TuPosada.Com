eb7b1d3fe5dc26e180b28fbe4ef922ea3bb23236
MACO <manuelcaceres455@gmail.com>
2022-02-21 16:29:28 -0400

    Registro comenzado

--- TuPosada.com/src/pages/registro.jsx
+++ TuPosada.com/src/pages/registro.jsx
@@ -1,18 +1,50 @@
 import { useState } from 'react'
-
+import './stylesR.css';
 function Reg(){
 
 
 
     return(
         <div className='reg'>
-            <h2 class="app-title">Expense Tracker</h2>
+            <h1 className='headerReg'>Ingrese su correo electronico</h1>
+            <div className= 'content'> 
 
-            <section class="balance-container center-column">
-                <p class="balance-title">Your balance</p>
-                <p class="balance-amount" id="totalBalance">$0.00</p>
-                <p class="palabra"> Mandalorian</p>
-            </section>
+                {/*
+                <div className='image'>
+                    <img source= 'tuposadalogo.com'/>
+                </div>
+                */}
+                <div className='form'>
+
+                    <div className="form-group">
+                        <label htmlFor="username">Correo electronico</label>
+                        <input type="text" name="username" placeholder="Ingrese su correo..." />
+                    </div>
+
+                    <h1 className='headerReg'>Crea una clave</h1>
+
+                    <div className="form-group">
+                        <label htmlFor="password">Contraseña</label>
+                        <input type="password" name="password" placeholder="Ingrese su contraseña..." />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="password">Confirme la cotraseña</label>
+                        <input type="password" name="password" placeholder="Ingrese su contraseña..." />
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className='footerReg'>
+                <button type='button' className='btnReg'>
+                    Registrarse
+                </button> 
+
+            </div>   
+
+
 
         </div>
     )
