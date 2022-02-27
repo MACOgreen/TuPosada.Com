e1b9383425fe39e295a308335b4847269e975013
MACO <manuelcaceres455@gmail.com>
2022-02-27 15:31:22 -0400

    ComienzoHomePage

--- TuPosada.com/src/pages/home/homePage.jsx
+++ TuPosada.com/src/pages/home/homePage.jsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
+import './stylesH.css';
+import logo from "./tuposadalogo.png";
 
 function Home(){
 
 
     return(
-        <div className='home'>
-            <h1>Este es el Home page</h1>
+        <div className='Home'>
+                < img className='ima' src={logo}/>
+                <div className='NavbarRyL'>  {/* Contenedor del NavBar para resgitro y login */}
+                    <ul className='Lista1'>{/* Links al register y login*/}
+                        <li><a   href={"/reg"}> Registrate</a></li>
+                        <li><a   href={'/login'}> Iniciar Sesión</a> </li>
+                    </ul>
+                </div>
 
+                <div className='NavbarByI'>  {/* Contenedor del NavBar para busqueda y informacion */}
+                    <ul className='Lista2'>{/* Links al busqueda y informacion*/}
+                        <li className='li2' ><a  className='a2'  href={"/reg"}> Busqueda de destino</a></li>
+                        <li className='li2' ><a   className='a2' href={'/login'}> Acerca de nosotros</a> </li>
+                    </ul>
+                </div>
+                
 
         </div>
 
