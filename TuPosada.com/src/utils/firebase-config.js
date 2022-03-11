b589621a7cfd8eba6f3da7eab2a32ba69c5e2e9f
MACO <manuelcaceres455@gmail.com>
2022-03-10 22:19:43 -0400

    FireBaseSincronizado

---
+++ TuPosada.com/src/utils/firebase-config.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+
+import "firebase/auth";
+import "firebase/storage";
+import "firebase/firestore";
+
+const firebaseConfig = { 
+    apiKey: "AIzaSyBtFAQWVvK_kl5LASad7KauZ0XWHkY8Q4Q",
+    authDomain: "tuposada-com.firebaseapp.com",
+    projectId: "tuposada-com",
+    storageBucket: "tuposada-com.appspot.com",
+    messagingSenderId: "386800650854",
+    appId: "1:386800650854:web:c492dd7b0695c88bb17ba7"
+}
+
+const app = firebase.initializeApp(firebaseConfig);
+
+// Database instance
+export const db = app.firestore();
+
+// Authentication instance
+export const auth = app.auth();
