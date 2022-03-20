82420e4ded18d5adb6e86c63f4ac8990ea5d7a32
Alejandro <alejogi20@gmail.com>
2022-03-20 19:09:26 -0400

    busqueda de ciudades implementada con firebase

--- TuPosada.com/src/pages/search-page/search-page.jsx
+++ TuPosada.com/src/pages/search-page/search-page.jsx
@@ -2,10 +2,21 @@ import { React, useState } from "react";
 import Data from "./mock_data.json";
 import "./stylesSearch.css";
 import List from "./list";
-
+import {app} from "../../utils/firebase-config";
 function Search() {
 
-    const [query, setQuery] = useState("");
+  const [query, setQuery] = useState("");
+
+  const onFileChange = (e) => {
+    const file = e.target.files[0]
+    const storageRef = app.storage().ref()
+    const fileRef = storageRef.child(file.name)
+    fileRef.put(file)
+  }
+  
+  const onSubmit = (e) => {
+    e.preventDefault();
+  }
 
   return (
     <div className="search-page">
@@ -13,7 +24,7 @@ function Search() {
       <div className="search">
         <input id="search-bar" placeholder="Busque su destino, posada o alojamiento" onChange={event => setQuery(event.target.value)}/>
       
-       
+          
 
       </div>
 
