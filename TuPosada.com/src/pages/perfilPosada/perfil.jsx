25937fb8d75dfc70dbba71ce1f02361443ec02bb
MACO <manuelcaceres455@gmail.com>
2022-03-27 12:04:42 -0400

    PerfilPosadaAvanzado

--- TuPosada.com/src/pages/perfilPosada/perfil.jsx
+++ TuPosada.com/src/pages/perfilPosada/perfil.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import "./perfilStyle.css"
 import Carousel from './carousel2/Carousel'
+import { UserContext } from "../../context/UserContext";
+import { useState,useContext} from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function Perfil() {
+  const { user, setUser} = useContext(UserContext);
+  const navigate = useNavigate();
+  const verificar=()=>{
+      if(user==null || user==""){
+        alert("Para realizar una reserva primero debe iniciar sesión.O registrarse si no se encuentra en el sistema.");
+        navigate("/login");
+      }
+      else{//Aqui se redirige a la vista de realizar reserva
+            
+
+      }
+  }
   return (
     <div className='info'>
         <h1 className='Tposada'> Nombre de la posada  </h1>
@@ -24,7 +39,7 @@ export default function Perfil() {
         <h2 className='Tposada' >Habitaciones disponibles</h2>
         <div className="pr">Aqui va la lista de habitaciones disponibles</div>
 
-        <button className='bo'>Realizar reserva</button>
+        <button className='bo' onClick={verificar}>Realizar reserva</button>
         
     </div>
   )
