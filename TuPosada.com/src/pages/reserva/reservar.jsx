baae878a2663bb6adad5d5b5a6619d1dee56a1bc
MACO <manuelcaceres455@gmail.com>
2022-03-28 21:05:55 -0400

    Actualizacion

--- TuPosada.com/src/pages/reserva/reservar.jsx
+++ TuPosada.com/src/pages/reserva/reservar.jsx
@@ -28,7 +28,7 @@ export default function Reservar() {
       console.log(posada);
       
       db.collection("posadas").doc(posada.undefined).update(posada);
-
+      alert("Fechas disponibles para reserva.");
       //Aqui se redirige a la vista de pago.
 
       //
@@ -39,7 +39,7 @@ export default function Reservar() {
         var mes2=1;
         var dia2=1;
         var disp ={};
-        
+        console.log("entra disponibilidad.")
         if(data.habitacion.toLowerCase()=="a"){
 
             mes1=parseInt(data.fLlega.split("-")[1]);
@@ -162,7 +162,7 @@ export default function Reservar() {
   return (
     <div className="Cuadro">
         <h1>Reserva </h1>
-        <form className='formX' onSubmit={handleSubmit((data)=>{disponibilidad(data)})}>  
+        <form className='formX' onSubmit={handleSubmit((data)=>{disponibilidad(data),setCheckOut(true) })}>  
 
             <div className="form-groupX">
                     <label htmlFor="habitacion">Indique la habitacion de su interes </label>
@@ -186,7 +186,7 @@ export default function Reservar() {
                 {checkout ? (
                     <PayPal/>
                 ) : (
-                        <button type='submit' className='btnX' onClick ={()=> {setCheckOut(true);} }>
+                        <button type='submit' className='btnX' >
                             Realizar Reserva
                         </button> 
                 )}
